9116f96a967e7938b2622db497cad391a144b98f
tez0ice <muhdzayyad91@gmail.com>
2024-01-15 09:45:54 +0800

    add maze lol

--- graphic.py
+++ graphic.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+import time
 
 class Window:
     def __init__(self,width,height):
@@ -88,3 +89,53 @@ class Cell:
         line = Line(Point(initial_mid_x,initial_mid_y),Point(initial_mid_x_next,initial_mid_y_next))
         self.__win.draw_line(line,"red")
     
+
+class Maze:
+    def __init__(self,x1,y1,num_rows,num_cols,cell_size_x,cell_size_y,win):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows 
+        self._num_cols = num_cols 
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+    
+    def _create_cells(self):
+
+        i = self._x1 
+        j = self._y1
+
+        for col in range(self._num_cols):
+            cell_col = []
+            for row in range(self._num_rows):
+                cell_col.append([])
+            self._cells.append(cell_col)
+        
+        # for col in range(len(self._cells)):
+        #     cell = Cell(self._win)
+        #     cell.draw(i,j,i+self._cell_size_x,j)
+        #     for row in range(col+1,len(self._cells[col])):
+        #         cell.draw(i,j,i+self._cell_size_x,j+self._cell_size_y)
+        #         j += self.cell_size_y
+        #     i += self.cell_size_x
+        
+        i = self._x1
+
+        for col in range(len(self._cells)):
+            j = self._y1
+            self._draw_cell(i,j)
+            for row in range(1,len(self._cells[col])):
+                self._draw_cell(i,j + self._cell_size_y)
+                j += self._cell_size_y
+            i += self._cell_size_x
+
+    def _draw_cell(self,i,j):   
+        cell = Cell(self._win)
+        cell.draw(i, j, self._cell_size_x + i, self._cell_size_y + j)
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.5)
+
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from graphic import Window,Point,Line,Cell
+from graphic import Window,Point,Line,Cell,Maze
 
 def main():
     win = Window(800,600)
-    cell1 = Cell(win,has_right=False)
-    cell2 = Cell(win,has_left=False,has_bot=False)
-    cell1.draw(50, 50, 100, 100)
-    cell2.draw(100, 50, 150, 100)
-    cell1.draw_move(cell2)
+    # cell1 = Cell(win,has_right=False)
+    # cell2 = Cell(win,has_left=False,has_bot=False)
+    # cell1.draw(50, 50, 100, 100)
+    # cell2.draw(100, 50, 150, 100)
+    # cell1.draw_move(cell2)
+    maze = Maze(50,50,5,5,50,50,win)
+    maze._create_cells()
     # win.draw_line(line,"black") 
     win.wait_for_close()
 
